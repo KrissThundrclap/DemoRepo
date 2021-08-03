47cd852fe24ef54fa268f9607116165dad912a56
Krish Shanbhag <kstar011235@gmail.com>
2021-08-03 20:11:46 +0530

    Fizzbuzz made easy

---
+++ fizzbuzz.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 100; i++){
+  let tbp = "";
+  if (i%3==0){tbp += "Fizz";}
+  if (i%5==0){tbp += "Buzz";}
+  if (i%3!==0 && i%5!==0) {console.log(i)}
+
+  if (tbp !== ""){console.log(tbp)}
+}
